e7fd659a4f0d97513cf0ce5729b891be06148c64
kazu-pa <kohachanbiyori0903@gmail.com>
2025-01-10 15:02:36 +0900

    Create contact.js

---
+++ js/contact.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // ヘッダーの読み込み
+  fetch("pages/header.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.querySelector("#header").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+
+  // フッターの読み込み
+  fetch("pages/footer.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.querySelector("#footer").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+});
